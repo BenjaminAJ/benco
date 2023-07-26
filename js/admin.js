fb646077604c8c5677402d5242556b92ee4c4ad2
BenjaminAJ <ajanakub30@gmail.com>
2023-07-26 09:22:58 +0100

    added add product functionality

--- js/admin.js
+++ js/admin.js
@@ -4,6 +4,7 @@ import {
   getFirestore,
   collection,
   getDocs,
+  addDoc
 } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -27,3 +28,32 @@ const db = getFirestore();
 //Collection Ref Products
 const colRef = collection(db, "products");
 
+
+addProduct.addEventListener('submit', (event)=>{
+    event.preventDefault();
+
+    //Do validation
+    let title = addProduct.productName.value;
+    let price = addProduct.productPrice.value;
+    let category = addProduct.productCategory.value;
+    let description = addProduct.productDescription.value;
+    let datePosted = new Date();
+    let sponsored = false;
+
+    addProduct.submitBTN.innerHTML = `
+    <div class="spinner-border" role="status">
+    <span class="visually-hidden">Loading...</span>
+  </div>
+    `;
+    addDoc(colRef, {title, price, category, description, datePosted, sponsored})
+    .then((result) => {
+        addProduct.submitBTN.innerHTML = `submit`;
+        addProduct.productName.value = '';
+        addProduct.productPrice.value = '';
+        addProduct.productCategory.value = '';
+        addProduct.productDescription.value = '';    
+        console.log('Product uploaded successfully');
+    }).catch((err) => {
+        console.error(err.message);
+    });
+})
